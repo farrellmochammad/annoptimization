4f29d143fe79b7dddea7d1dbc0dd1efb0ce6d3a1
tbengkel <farrellmochammad@gmail.com>
2018-10-28 19:46:32 +0700

    First Commit

---
+++ tugas2.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import random
+
+dataframe = pd.read_csv('data.csv')
+f = open("column1.txt","r")
+
+column = f.read()
+columnname = column.split("\t")
+columnname[len(columnname)-1] = 'fractal_dimension_worst'
+
+def generatehiddenlayer(hiddenlayer,countweight):
+        hiddenneuron = []
+        for i in range(0,hiddenlayer):
+                neuron = []
+                for j in range(0,countweight):
+                        neuron.append(0)
+                hiddenneuron.append(neuron)
+        return hiddenneuron
+
+def generateoutputlayer(outputlayer,countweight):
+        outputneuron = []
+        for i in range(0,outputlayer):
+                neuron = []
+                for j in range(0,countweight):
+                        neuron.append(0)
+                outputneuron.append(neuron)
+        return outputneuron
+
+def testing(dataframe,outputneuron,hiddenneuron,biashidden,biasoutput):
+        TP = 0
+        FN = 0
+        for i in range(0,len(dataframe[columnname[0]])):
+                neuronvalue = []
+                for j in range(2,len(columnname)):
+                        data = (dataframe[columnname[j]][i] - min(dataframe[columnname[j]])) / (max(dataframe[columnname[j]]) - min(dataframe[columnname[j]])) 
+                        for k in range(0,len(hiddenneuron)):
+                                hiddenneuron[k][j-2] = (data*random.uniform(0,1))
+                for l in range(0,len(hiddenneuron)):
+                        neuronvalue.append(sum(hiddenneuron[l])-biashidden[l])
+                for m in range(0,len(neuronvalue)):
+                        for n in range(0,len(outputneuron)):
+                                outputneuron[n][m] = neuronvalue[m] * random.uniform(0,1)
+                yes = 0
+                no = 0
+                for o in range(0,len(outputneuron)):
+                        if (o==0):
+                                yes += sum(outputneuron[o]) - biasoutput[o]
+                        else:
+                                no += sum(outputneuron[o]) - biasoutput[o]
+                if (yes>no) :
+                        if dataframe[columnname[1]][i] == "M":
+                                TP += 1
+                else :
+                        if dataframe[columnname[1]][i] == "B":
+                                FN += 1
+        return TP+FN
+
+def algoritmabco(dataframe,totalbee,NC):
+        bee = []
+        solution = []
+        #hiddenneuron = 5
+        outputneuron = 2
+        for i in range(0,totalbee):
+                solusi = []
+                for k in range(0,NC+outputneuron):
+                        solusi.append(random.uniform(0,1))
+                bee.append(solusi)
+        for j in range(0,totalbee):
+                print "Solusi Bee ",j+1
+                solution.append(testing(dataframe,generateoutputlayer(outputneuron,NC),generatehiddenlayer(NC,30),bee[j][:NC],bee[j][NC:])/float(len(dataframe[columnname[0]]))*100)
+                print "Selesai"
+        print "Solusi yang selama ini dihasilkan ",solution
+                        
+
+algoritmabco(dataframe,10,5)
+        
+#print "Akurasi = ",testing(dataframe,generateoutputlayer(2,5),generatehiddenlayer(5,30),[0,0,0,0,0,0,0])/float(len(dataframe[columnname[0]]))*100,"%"
